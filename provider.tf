af3d3ac77b761ae1f59519ec7dd059d9de1bd7b4
Alekhyavannar6637 <147797116+Alekhyavannar6637@users.noreply.github.com>
2024-01-30 17:03:47 +0530

    Update provider.tf
    jjhhkk

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
-  access_key =  "AKIA32PH3AC5Y7QXBIPN"
-  secret_key = "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"
+  region = "ap-southeast-2"
+  access_key =  "AKIA4MTWKGOMPZFPITY4"
+  secret_key = "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"
 }
